4b249a3f2b1abe594a35c90078e5c139caec37ec
sushant <sushant-demn>
2026-02-12 21:13:42 +0530

    Add project files.

---
+++ MusicStreamingApp.cpp
@@ -0,0 +1,12 @@
+﻿// MusicStreamingApp.cpp : Defines the entry point for the application.
+//
+
+#include "MusicStreamingApp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ MusicStreamingApp.h
@@ -0,0 +1,8 @@
+﻿// MusicStreamingApp.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
